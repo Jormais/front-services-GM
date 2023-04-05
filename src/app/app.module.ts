b712897805a9761989d24c2b2a37738e8c086870
unknown <jonaybrito15@gmail.com>
2023-04-05 14:24:54 +0200

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormularioPeticionServiciosComponent } from './formulario-peticion-servicios/formulario-peticion-servicios.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioPeticionServiciosComponent
+    FormularioPeticionServiciosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
